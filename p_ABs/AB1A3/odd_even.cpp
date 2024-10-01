f98302cff55244fcf06290a3d5e73d3394a66b15
Tim2othy <150283209+Tim2othy@users.noreply.github.com>
2024-10-01 15:27:17 +0200

    odd_even fertig

--- p_ABs/AB1A3/odd_even.cpp
+++ p_ABs/AB1A3/odd_even.cpp
@@ -1,5 +1,14 @@
 # include <iostream>
 
 int main() {
+    int n = 13;
 
+    if ((n % 2) == 0)
+    {
+        std::cout << n / 2;
+    }
+    else
+    {
+        std::cout << (n + 1) / 2;
+    }
 }
