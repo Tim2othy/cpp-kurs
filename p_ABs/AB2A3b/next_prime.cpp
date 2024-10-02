4aa2315c3857a661049101413be3778994b77747
Tim2othy <150283209+Tim2othy@users.noreply.github.com>
2024-10-02 16:03:27 +0200

    why do = and == look so similar !!!

--- p_ABs/AB2A3b/next_prime.cpp
+++ p_ABs/AB2A3b/next_prime.cpp
@@ -2,24 +2,40 @@
 
 int main()
 {
-
-
-    for (int input = 20000; ; input++)
+    for (int input = 20000; true; input++)
     {
+        int is_prime = 10;
+
+        int output = input;
+
+        std::cout << "At the start" << std::endl;
+
 
         for (int n = 2; n * n < input; n++)
         {
+            std::cout << "I'm loop" << std::endl;
+
             if ((input % n) == 0)
             {
-                std::cout << "Not prime" << std::endl;
+                is_prime = 0;
+                std::cout << "Jetzt Break" << std::endl;
+
                 break;
             }
-            std::cout << "Could be prime" << std::endl;
+            std::cout << "Hierhin?" << std::endl;
+
+            is_prime = 1;
         }
 
+        std::cout << "Is_prime = " << is_prime << std::endl;
 
 
+        if (is_prime == 1)
+        {
+            std::cout << "Passiert?" << std::endl;
 
-
+            std::cout << "Der output ist: " << output;
+            break;
+        }
     }
 }
